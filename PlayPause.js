3ef0dfc95392e401781857d1e85cf87e287c1b3e
Holger <holgerholwerda@gmail.com>
2021-04-30 10:58:17 +0200

    playpause

--- PlayPause.js
+++ PlayPause.js
@@ -1,10 +1,16 @@
 
-var song = document.getElementById("sound"); 
+var result = results[0]
+var song = document.getElementById("sound");
 
-function playSong() { 
-  song.play(); 
-} 
+function playSong() {
+  song.play();
+}
+function pauseSong() {
+  song.pause();
+}
 
-function pauseSong() { 
-  song.pause(); 
-} 
+if (result == Play) {
+  playSong()
+} else {
+  pauseSong()
+}
